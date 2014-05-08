8e1b4b46e381efc5952a82b62d15c272e64657b8
Kyle Poole <poolekyle@gmail.com>
2014-05-08 12:34:24 -0700

    More preparations/sponsors for soft launch.

--- register2014.php
+++ register2014.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
   // database login credentials are stored outside web-facing space.
   // We may have to change this absolute path when the site goes live,
   // ../credentials.php doesn't seem to work in this server configuration.
@@ -49,179 +46,89 @@ ini_set('display_errors', '1');
       }
     }
 
-  if($errors == null) {
-  	$LinkID = mysql_connect($req_server, $req_username, $req_password);
+    if($errors == null) {
+    	$LinkID = mysql_connect($req_server, $req_username, $req_password);
 
-  	// Die if no connect
-  	if (!$LinkID) {
-  		die('Could not connect: ' . mysql_error());
-  	}
+    	// Die if no connect
+    	if (!$LinkID) {
+    		die('Could not connect: ' . mysql_error());
+    	}
 
-  	// Choose the DB and run a query.
-  	mysql_select_db('capjune2014');
-	
-	
-	 // Sanitize inputs for database
-       // (not implemented yet and might not be - we're using mysqli prepared statements)
-   
+    	// Choose the DB and run a query.
+    	mysql_select_db('capjune2014');
+  	
+  		$query = "select * from reg_tbl where email='$email'";
 
-       // Add to registration table:
+      $result = mysql_query($query);
 
-       // Setup mysqli
-       //$mysqli = new mysqli($db_host, $db_username, $db_password, "capjune2013");
-
-
-      // if (mysqli_connect_errno()) {
-       //  printf("mysql connection failed: %s\r\n<br>", mysqli_connect_error());
-       //  exit;
-       //}
-
-       // Setup SQL statement
-    //   $sql = "INSERT INTO registration ".
-    //          "(title, role, firstName, lastName, company, email, regdate) ".
-    //          "values ".
-    //          "(?, ?, ?, ?, ?, ?, SYSDATE());";
-
-	
-		$query = "select * from reg_tbl where email='$email'";
-	  echo $email;
-
-    $result = mysql_query($query);
-
-		if (mysql_num_rows($result)) {
-			$errors .= "This <strong>E-Mail Address</strong> has been registered already. Please contact <a href=\"mailto:cap2014@cs.camosun.bc.ca\">cap2014@cs.camosun.bc.ca</a> if an error has occured. <br/>\r\n";
-            //exit();
-		} else {
-		//if (mysql_num_rows($result) >= 1) {
-            //email is taken
-            //header('location:reg.php?error=email address already registered. please try another one');
-		//	$errors .= "This <strong>E-Mail Address</strong> has been registered already. Please contact <a href=\"mailto:cap2014@cs.camosun.bc.ca\">cap2014@cs.camosun.bc.ca</a> if an error has occured. <br/>\r\n";
-         //   exit();
-		//}	
-		
-			// Assemble the SQL query
-			$query = "INSERT INTO reg_tbl (first_name, last_name, org_comp_name, email, phone) VALUES ('$firstName', '$lastName','$company','$email', '$phone')";
-			// Excute sql query
-			$result = mysql_query($query);
-			if ($result) {
-				//echo "Register success";
-				//$msg = "User Created Successfully.";
-			} elseif (!$result) { // If the query returned error, display error the msg
-				echo "Could not successfully run query ($query) from DB ";
-				//exit;
-			}
-		}	
-				
-    // Insert the record
-    // if ($prepared = $mysqli->prepare($sql)) {
-    //   $prepared->bind_param("ssssss", $title, $role, $firstName, $lastName, $company, $email);
-  	//			 if (!$prepared->execute()) {
-  	//					if ($prepared->errno == "1062") {
-  	//						$errors .= "This <strong>E-Mail Address</strong> has been registered already. Please contact <a href=\"mailto:cap2014@cs.camosun.bc.ca\">cap2014@cs.camosun.bc.ca</a> if an error has occured. <br/>\r\n";
-  	//				}
-  	//		 }
-    //     $prepared->close();
-    //   } else {
-    //     printf("Error in prepared statement: %s\r\n<br>",$mysqli->error);
-    //   }
+  		if (mysql_num_rows($result)) {
+  			$errors .= "This <strong>E-Mail Address</strong> has been registered already. Please contact <a href=\"mailto:capstone2014@cs.camosun.bc.ca\">capstone2014@cs.camosun.bc.ca</a> if an error has occured. <br/>\r\n";
+		  } else {
+  			// Assemble the SQL query
+  			$query = "INSERT INTO reg_tbl (first_name, last_name, org_comp_name, email, phone) VALUES ('$firstName', '$lastName','$company','$email', '$phone')";
+  			// Excute sql query
+  			$result = mysql_query($query);
+  			if ($result) {
+  				//echo "Register success";
+  				//$msg = "User Created Successfully.";
+  			} elseif (!$result) { 
+          // If the query returned error, display error the msg
+  				$errors .= "Could not successfully run query ($query) from DB ";
+  				//exit;
+  			}
+		  }	
+    }
   }
-			if($errors == null) {	
 
-			// Set up Registration Committee notification email
+  if($errors != null) {
+    // If there are errors then return them to the user.
+    echo $errors;
+    return;
+  }
 
-			// Set up registrant name
-	   $name = ""; 
-			//if ($title!="") {
-			 //    $name .= $title. " ";
-			//}
-			   $name .= $firstName. " ". $lastName;
+  if($errors == null) {	
+    // Set up Registration Committee notification email
+  	// Set up registrant name
+    $name = ""; 
+    $name .= $firstName. " ". $lastName;
 
-			   // Set up message body
-			   $data  = "--b1_fce20d8377a30a7c38bd2a920598ae8f
-		Content-Type: text/plain; charset = \"UTF-8\"
-		Content-Transfer-Encoding: quoted-printable\r\n\r\n";
-			   $data .= "New Capstone 2014 Registration:\r\n\r\n";
-			   $data .= $name."\r\n";
-			   //$data .= "Role: $role\r\n";
-			$data .= "Company: $company\r\n";
-			$data .= "E-Mail: $email \r\n";
+    // Set up message body
+  	// Set subject
+  	$subject = "New Capstone 2014 Registration (" .substr($firstName,0,1). " " .$lastName. ")\r\n";
 
-			// Set subject
-			$subject = "New Capstone 2014 Registration (" .substr($firstName,0,1). " " .$lastName. ")\r\n";
-	
-	// Set headers		
-	$headers = 'From: capstone2014@cs.camosun.bc.ca' . "\r\n" .
-    	           'Reply-To: capstone2014@cs.camosun.bc.ca' . "\r\n" .
-    		    'X-Mailer: PHP/' . phpversion()."\r\n".
-                  'MIME-Version: 1.0'."\r\n".
-                  'Content-Type: multipart/alternative;'."\r\n".
-                  '    boundary="b1_fce20d8377a30a7c38bd2a920598ae8f"';
-				
-       // Send notification email(s) to Registration Committee
+    // Set headers		
+    $headers =  'From: capstone2014@cs.camosun.bc.ca' . "\r\n" .
+    	          'Reply-To: capstone2014@cs.camosun.bc.ca' . "\r\n" .
+        		    'X-Mailer: PHP/' . phpversion()."\r\n".
+                'MIME-Version: 1.0'."\r\n";
+  		
+     // Send notification email(s) to Registration Committee
+     $mailSent = true;
 
-	//mail("capstone2014@cs.camosun.bc.ca", $subject, $data, $headers);
-      // mail("gerald@codegods.ca", $subject, $data, $headers); 
-       $mailSent = true;
+     // Set up registrant notification email   
+     // Set subject
+     $subject = "Camosun College Technologies Capstone 2014 Registration - Confirmation\r\n";
 
-       // Set up registrant notification email   
+     // Set up message body
+     $data  = "
+              Hello $name!  Thank you for registering for the Camosun College Technologies Capstone Symposium 2014.
 
-       // Set subject
-       $subject = "Camosun College Technologies Capstone 2014 Registration - Confirmation\r\n";
-    
-       // Set up message body
-       $data  = "--b1_fce20d8377a30a7c38bd2a920598ae8f
-Content-Type: text/plain; charset = \"UTF-8\"
-Content-Transfer-Encoding: quoted-printable
+              The registration details we recorded are:
 
+              Name: $name
+              Company: $company
+              email: $email
 
-Hello $name!  Thank you for registering for the Camosun College Technologies Capstone Symposium 2014.
+              The Symposium will be held at Pacific Forestry Centre, 506 Burnside Road West, Victoria BC. from 9am to 6pm on Wednesday, June 19, 2014.
 
-The registration details we recorded are:
+              You will be notified by email if any important schedule changes occur.
 
-Name: $name
-Company: $company
-email: $email
+              If you have any questions, please contact us at capstone2014@cs.camosun.bc.ca.
+              ";
 
-The Symposium will be held at Pacific Forestry Centre, 506 Burnside Road West, Victoria BC. from 9am to 6pm on Wednesday, June 19, 2014.
-
-You will be notified by email if any important schedule changes occur.
-
-If you have any questions, please contact us at capstone2014@cs.camosun.bc.ca.
-
----------------------------------------------------------------------------------
-The Camosun College Technologies Capstone Symposium will be attended by movers and shakers in the tech sector.  This is an opportunity to reach them!  Sponsorship opportunities range from a simple business card ad to a tradeshow booth!  Contact us at cap2014@cs.camosun.bc.ca to learn more...\r\n\r\n
-\r\n
-
---b1_fce20d8377a30a7c38bd2a920598ae8f
-Content-Type: text/html; charset = \"UTF-8\"
-Content-Transfer-Encoding: quoted-printable
-<body style=\"font-family:Segoe UI,Myriad;\">
-<p>Hello $name!  Thank you for registering for the Camosun College Technologies Capstone Symposium 2014.</p>
-<p>The registration details we recorded are:</p>
-<div style=\"margin-left:48px;\">
-Name: $name<br>
-Company: $company<br>
-email: $email<br>
-</div>
-<p>The Symposium will be held at 
-<div style=\"margin-left:48px;\">Pacific Forestry Centre<br>506 Burnside Road West<br>Victoria BC</div>
-<br>from 9am to 6pm on Wednesday, June 19, 2014.</p>
-<p>You will be notified by email if any important schedule changes occur.\r\n</p>
-<p>If you have any questions, please contact us at <a href=\"mailto:capstone2014@cs.camosun.bc.ca\">capstone2014@cs.camosun.bc.ca</a>.</p>
-<hr>
-<p>The Camosun College Technologies Capstone Symposium will be attended by movers and shakers and up-and-comers 
-in the tech sector.  This is an opportunity to reach them!  Sponsorship opportunities range from a simple business 
-card ad to a tradeshow booth!  Contact us at <a href=\"mailto:cap2014@cs.camosun.bc.ca\">cap2014@cs.camosun.bc.ca</a> to learn more...</p>
-</body>
---b1_fce20d8377a30a7c38bd2a920598ae8f--
-
-
-";
-
-       // Send registrant notification email      
-       mail($email, $subject, $data, $headers);
-			} 
-		}
+    // Send registrant notification email      
+    mail($email, $subject, $data, $headers);
+	} 
 
 //Validate Email
 function valid_email($email)
@@ -232,7 +139,6 @@ function valid_email($email)
 	//Check mail server is up
 	else
 	{
-
 		//get mail server address
 		list($username, $domain) = split("@",$email);
 		if(@getmxrr($domain, $mxhost))
@@ -249,13 +155,15 @@ function valid_email($email)
 
 	return false;
 }
-  if(!$mailSent) {    
-    if ($tooLate) {
-      echo("<p>Registration is now closed. Thank you for your support of the 2014 Capstone Symposium.</p>");
-    }
 
-    if(!empty($errors)) {
-      echo "<p>".$errors."</p>";
-    }
+if(!$mailSent) {    
+  if ($tooLate) {
+    echo("<p>Registration is now closed. Thank you for your support of the 2014 Capstone Symposium.</p>");
   }
+
+  if(!empty($errors)) {
+    echo "<p>".$errors."</p>";
+  }
+}
+
 ?> 
